7ae4a1ab13fd36b1c4f94c052275bff0e6973fa1
huang <356071707@qq.com>
2022-11-14 22:12:39 +0800

    Initial Commit

---
+++ wo1dsss/wo1dsssApp.swift
@@ -0,0 +1,17 @@
+//
+//  wo1dsssApp.swift
+//  wo1dsss
+//
+//  Created by hzx on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct wo1dsssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
